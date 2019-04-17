8c0293992951921786ab86e1d87c637ed2ad7baf
v1xingyue <qixingyue@126.com>
2019-04-17 15:20:37 +0800

    简单函数拆分

--- m.go
+++ m.go
@@ -6,6 +6,12 @@ import (
 	"log"
 	"net"
 	"os"
+	"time"
+)
+
+const (
+	ModePassword int = iota
+	ModeKey      int = iota
 )
 
 func nilCallBack(hostname string, remote net.Addr, key ssh.PublicKey) error {
@@ -18,15 +24,7 @@ func errorCallBack(err error, msg string) {
 	}
 }
 
-func loginWithPassword(password string, host string, user string) {
-	client, err := ssh.Dial("tcp", host, &ssh.ClientConfig{
-		User:            user,
-		Auth:            []ssh.AuthMethod{ssh.Password(password)},
-		HostKeyCallback: nilCallBack,
-	})
-	errorCallBack(err, "dial")
-	session, err := client.NewSession()
-	errorCallBack(err, "new session")
+func runSession(session *ssh.Session) {
 	defer session.Close()
 	session.Stdout = os.Stdout
 	session.Stderr = os.Stderr
@@ -41,16 +39,29 @@ func loginWithPassword(password string, host string, user string) {
 	w, h, _ := terminal.GetSize(termFD)
 	termState, _ := terminal.MakeRaw(termFD)
 	defer terminal.Restore(termFD, termState)
-	err = session.RequestPty("xterm-256color", h, w, modes)
+	err := session.RequestPty("xterm-256color", h, w, modes)
 	errorCallBack(err, "request pty")
 	err = session.Shell()
 	errorCallBack(err, "start shell")
 	err = session.Wait()
 	errorCallBack(err, "return")
+
+}
+
+func loginWithPassword(password string, host string, user string) {
+	client, err := ssh.Dial("tcp", host, &ssh.ClientConfig{
+		User:            user,
+		Auth:            []ssh.AuthMethod{ssh.Password(password)},
+		HostKeyCallback: nilCallBack,
+		Timeout:         30 * time.Second,
+	})
+	errorCallBack(err, "dial")
+	session, err := client.NewSession()
+	errorCallBack(err, "client new session")
+	runSession(session)
 }
 
 func loginWithKey(keystr string, host string, user string) {
-
 	auth := make([]ssh.AuthMethod, 0)
 	signer, err := ssh.ParsePrivateKey([]byte(keystr))
 	if err != nil {
@@ -62,31 +73,14 @@ func loginWithKey(keystr string, host string, user string) {
 		User:            user,
 		Auth:            auth,
 		HostKeyCallback: nilCallBack,
+		Timeout:         30 * time.Second,
 	})
 	errorCallBack(err, "dial")
 	session, err := client.NewSession()
-	errorCallBack(err, "new session")
-	defer session.Close()
-	session.Stdout = os.Stdout
-	session.Stderr = os.Stderr
-	session.Stdin = os.Stdin
-	modes := ssh.TerminalModes{
-		ssh.ECHO:          1,
-		ssh.ECHOCTL:       0,
-		ssh.TTY_OP_ISPEED: 14400,
-		ssh.TTY_OP_OSPEED: 14400,
-	}
-	termFD := int(os.Stdin.Fd())
-	w, h, _ := terminal.GetSize(termFD)
-	termState, _ := terminal.MakeRaw(termFD)
-	defer terminal.Restore(termFD, termState)
-	err = session.RequestPty("xterm-256color", h, w, modes)
-	errorCallBack(err, "request pty")
-	err = session.Shell()
-	errorCallBack(err, "start shell")
-	err = session.Wait()
-	errorCallBack(err, "return")
+	errorCallBack(err, "client new session")
+	runSession(session)
 }
 
 func main() {
+	loginWithPassword(Password, Host, User)
 }
