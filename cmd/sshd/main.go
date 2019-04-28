a5ce61a634402f14c9d44813f7e25a8f7eb8131b
v1xingyue <qixingyue@126.com>
2019-04-28 11:32:21 +0800

    保证sshd root 用户运行

--- cmd/sshd/main.go
+++ cmd/sshd/main.go
@@ -3,8 +3,14 @@ package main
 import (
 	_ "bbssh/options/sshd"
 	"bbssh/sshd"
+	"bbssh/user"
+	"log"
 )
 
 func main() {
-	sshd.StartSshd()
+	if user.IsRoot() {
+		sshd.StartSshd()
+	} else {
+		log.Fatal("You Must Run This Under Root User")
+	}
 }
